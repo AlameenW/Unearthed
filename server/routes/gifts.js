bc7c1f3824e883a5f2f35a2d8b118ada661573d1
Alameen <Adekualameem@gmail.com>
2025-09-28 16:17:16 -0500

    Postgres Database connected, Issues with gift details page

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -2,15 +2,13 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import giftData from "../data/giftData.js";
-
+import GiftsController from "../controllers/gifts.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json(giftData);
-});
+router.get("/", GiftsController.getGifts);
 
 router.get("/api/:giftId", (req, res) => {
   // GET /gifts/api/1 - Return specific gift as JSON
